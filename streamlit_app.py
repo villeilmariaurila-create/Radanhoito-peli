7759f6882cdb7f4605b0dfb30f5654314df49633
villeilmariaurila-create <villeilmariaurila@gmail.com>
2026-09-14 22:30:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.html(open("game.html", encoding="utf-8").read(), height=600)
+# Ladataan game.html tiedosto
+with open("game.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
+
+# Näytetään HTML Streamlitissä
+components.html(html_code, height=600, scrolling=True)
